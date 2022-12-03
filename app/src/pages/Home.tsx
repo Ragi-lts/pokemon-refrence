6227ec851f15b68d127bd9af8a20f7a25cab392a
Ragi-lts <yuusan1694301@gmail.com>
2022-12-03 22:20:06 +0900

    first commit

---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { useState } from "react";
+import MenuBar from "../components/Home/MenuBar";
+
+function App() {
+  return (
+    <div className="App">
+      <MenuBar />
+    </div>
+  );
+}
+
+export default App;
